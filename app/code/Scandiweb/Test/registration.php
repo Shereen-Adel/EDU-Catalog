b18809ac10ac4fa5c438c8dbfd2d96a90ea69d4d
Shereen Adel <shereen.adel@scandiweb.com>
2022-08-26 17:01:32 +0200

    new commit

---
+++ app/code/Scandiweb/Test/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Scandiweb_Test',
+    __DIR__
+);
